752ae56fae474dd1ed25a02ef15ef60ed5f5e6a8
Mohamed Amine <amine339@outlook.fr>
2024-10-31 15:33:54 +0100

    Add answers for question remaining questions

--- 2-Social Media Platform/tst.js
+++ 2-Social Media Platform/tst.js
@@ -1,3 +1,17 @@
 let date = new Date().getDate() - 30;
-let date2 =  // set Date is to set the day you're in 
-console.log(date2);
+let date2 = console.log(date2); // set Date is to set the day you're in
+
+db.post.updateOne(
+  { _id: "post2" },
+  {
+    $set: {
+      likes: {
+        $cond: {
+          if: { $in: [userId, "$tags"] },
+          then: { $setDifference: ["$tags", [userId]] },
+          else: { $concatArrays: ["$tags", [userId]] },
+        }
+      }
+    }
+  }
+);
